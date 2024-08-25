250e96253cdb778382dcc5a4a3c1a1c7c5ccd396
toriqultonu <toriqultonu22@gmail.com>
2024-08-25 12:38:33 +0600

    update

--- app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
+++ app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
@@ -70,16 +70,43 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
 
     object PoseLandmark {
 
+        const val NOSE = 0
+        const val LEFT_EYE_INNER = 1
+        const val LEFT_EYE = 2
+        const val LEFT_EYE_OUTER = 3
+        const val RIGHT_EYE_INNER = 4
+        const val RIGHT_EYE = 5
+        const val RIGHT_EYE_OUTER = 6
+        const val LEFT_EAR = 7
+        const val RIGHT_EAR = 8
+        const val MOUTH_LEFT = 9
+        const val MOUTH_RIGHT = 10
         const val LEFT_SHOULDER = 11
-        const val LEFT_ELBOW = 13
-        const val LEFT_WRIST = 15
-
         const val RIGHT_SHOULDER = 12
+        const val LEFT_ELBOW = 13
         const val RIGHT_ELBOW = 14
+        const val LEFT_WRIST = 15
         const val RIGHT_WRIST = 16
+        const val LEFT_PINKY = 17
+        const val RIGHT_PINKY = 18
+        const val LEFT_INDEX = 19
+        const val RIGHT_INDEX = 20
+        const val LEFT_THUMB = 21
+        const val RIGHT_THUMB = 22
+        const val LEFT_HIP = 23
+        const val RIGHT_HIP = 24
+        const val LEFT_KNEE = 25
+        const val RIGHT_KNEE = 26
+        const val LEFT_ANKLE = 27
+        const val RIGHT_ANKLE = 28
+        const val LEFT_HEEL = 29
+        const val RIGHT_HEEL = 30
+        const val LEFT_FOOT_INDEX = 31
+        const val RIGHT_FOOT_INDEX = 32
     }
 
-    
+
+
 
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
@@ -112,6 +139,7 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
                 val rightWrist = landmark[PoseLandmark.RIGHT_WRIST]
 
 
+
                 // Calculate and draw angles
 //                val leftElbow = landmark[PoseLandmarker.LEFT_ELBOW]
 //                val leftShoulder = landmark[PoseLandmarker.LEFT_SHOULDER]
@@ -121,8 +149,10 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
 //                val rightShoulder = landmark[PoseLandmarker.RIGHT_SHOULDER]
 //                val rightWrist = landmark[PoseLandmarker.RIGHT_WRIST]
 
-                val leftElbowAngle = calculateAngle(leftShoulder, leftElbow, leftWrist)
-                val rightElbowAngle = calculateAngle(rightShoulder, rightElbow, rightWrist)
+                val leftElbowAngle = calculateAngle(landmark[PoseLandmark.LEFT_SHOULDER], landmark[PoseLandmark.LEFT_ELBOW], landmark[PoseLandmark.LEFT_WRIST])
+                val rightElbowAngle = calculateAngle(landmark[PoseLandmark.RIGHT_SHOULDER], landmark[PoseLandmark.RIGHT_ELBOW], landmark[PoseLandmark.RIGHT_WRIST])
+
+
 
                 // Draw left elbow angle
                 canvas.drawText(
